7e889ff1b478bfa0ff580ea9f7604e54eda86795
Alex Izmaylov <18300654+DekhanFraser@users.noreply.github.com>
2024-01-30 10:38:53 +1300

    JS Completed sumAmm exercise
    Learned about number identification and parsing.

--- foundation/javascript-exercises/05_sumAll/sumAll.js
+++ foundation/javascript-exercises/05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let sum = 0;
+    if (start > end) {
+        [start, end] = [end, start];
+    }
+    if (start < 0 || end < 0 || !Number.isFinite(start) || !Number.isFinite(end)) {
+        return "ERROR";
+    }
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
 
+    return sum;
 };
 
 // Do not edit below this line
