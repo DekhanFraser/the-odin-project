8d3b0f9b1d90c53c46e65aca9e93bca34a4800a7
Alex Izmaylov <18300654+DekhanFraser@users.noreply.github.com>
2024-01-29 20:58:27 +1300

    JS Completed reverseString

--- foundation/javascript-exercises/03_reverseString/reverseString.js
+++ foundation/javascript-exercises/03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let finalString = '';
+    for (let i = str.length; i > 0 ; i--) {
+        finalString += str[i-1];
+    }
+    return finalString;
 };
 
 // Do not edit below this line
