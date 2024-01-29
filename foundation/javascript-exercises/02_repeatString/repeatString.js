30b49c1e5ed5e755d466f71e8dcb0355ceacffaa
Alex Izmaylov <18300654+DekhanFraser@users.noreply.github.com>
2024-01-29 20:48:57 +1300

    JS Completed 01_helloWorld & 02_repeatString

--- foundation/javascript-exercises/02_repeatString/repeatString.js
+++ foundation/javascript-exercises/02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(message, numRepeats) {
+    let finalString = '';
+    if (numRepeats < 0) {
+        return "ERROR";
+    }
+    for (let i = 0; i < numRepeats; i++) {
+        finalString += message;
+    }
+    return finalString;
 };
 
 // Do not edit below this line
