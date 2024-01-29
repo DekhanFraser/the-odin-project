3c36dcca5ea44203a99ad73e4d6fff263e949f94
Alex Izmaylov <18300654+DekhanFraser@users.noreply.github.com>
2024-01-30 11:24:09 +1300

    JS Completed tempConversion exercise

--- foundation/javascript-exercises/07_tempConversion/tempConversion.js
+++ foundation/javascript-exercises/07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+    return parseFloat(((temp - 32)*5/9).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  return parseFloat((temp*9/5 + 32).toFixed(1));
 };
 
 // Do not edit below this line
