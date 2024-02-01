56823193b316ebdeb7dc8c25feeddbdcf73b942c
Alex Izmaylov <18300654+DekhanFraser@users.noreply.github.com>
2024-02-02 11:21:24 +1300

    JS solved 10_fibonacci exercise

--- foundation/javascript-exercises/10_fibonacci/fibonacci.js
+++ foundation/javascript-exercises/10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if(n < 0) return "OOPS";
+    let a = 0, b = 1, c;
+    if (n==0) {
+        return a;
+    }
+    for (let i = 2; i <= n; i++) {
+         c = a + b;
+         a = b;
+         b = c;
+    }
+    return b;
 };
 
 // Do not edit below this line
