c970e979cdacc9be75c4de99ad37ab2c94a2d1e6
Alex Izmaylov <18300654+DekhanFraser@users.noreply.github.com>
2024-02-02 09:43:09 +1300

    JS Completed calculator exercise

--- foundation/javascript-exercises/08_calculator/calculator.js
+++ foundation/javascript-exercises/08_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((accumulator, value) => accumulator + value, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((accumulator, value) => accumulator * value);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a == 0) {
+    return 1;
+  }
+  return a * factorial(a - 1);
 };
 
 // Do not edit below this line
