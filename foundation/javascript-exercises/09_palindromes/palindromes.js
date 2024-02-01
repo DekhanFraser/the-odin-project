f15e59130f797fc9d9fc8519c6b54e16d5b39734
Alex Izmaylov <18300654+DekhanFraser@users.noreply.github.com>
2024-02-02 10:56:00 +1300

    JS solved 09_palindromes exercise
    Had to dive into and finde resources on regular expressions on this one
    Did extensive testing to find the right expression to search for

--- foundation/javascript-exercises/09_palindromes/palindromes.js
+++ foundation/javascript-exercises/09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const toEvaluate = str.replace(/\W+/g, '').toLowerCase();
+    let arrayLength = toEvaluate.length;
+    for (let index = 0; index < toEvaluate.length/2; index++) {
+        if (toEvaluate[index]!=toEvaluate[arrayLength-1-index]) {
+            return false;
+        }        
+    }
+    return true;
 };
+
+console.log(palindromes('racecar'));
 
 // Do not edit below this line
 module.exports = palindromes;
