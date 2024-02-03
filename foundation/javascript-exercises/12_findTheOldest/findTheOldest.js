b134ee124d316292c8be83282db88a5df1afe4e0
Alex Izmaylov <18300654+DekhanFraser@users.noreply.github.com>
2024-02-03 15:32:22 +1300

    JS Solved 12_findTheOldest exercise
    Had first experiences with the Date() constructor and functions.

--- foundation/javascript-exercises/12_findTheOldest/findTheOldest.js
+++ foundation/javascript-exercises/12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let oldestAge = 0, oldestIndex;
+    for (let index = 0; index < arr.length; index++) {
+        let age;
+        if (arr[index].yearOfDeath == undefined) {
+            age = new Date().getFullYear() - arr[index].yearOfBirth;
+        }
+        else {
+            age = arr[index].yearOfDeath - arr[index].yearOfBirth;
+        }
+        if (age > oldestAge) {
+            oldestAge = age;
+            oldestIndex = index;
+        }
+    }
+    return arr[oldestIndex];
 };
 
 // Do not edit below this line
